09524519c8aee782074973e6fbd0645d433f6666
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-16 17:04:12 -0400

    update to add networking

--- main.tf
+++ main.tf
@@ -18,6 +18,65 @@ resource "azurerm_resource_group" "aiof_rg" {
   }
 }
 
+
+resource "azurerm_network_security_group" "aiof_vnet_nsg" {
+  name                = "vnet-${var.env}-${var.location}-nsg"
+  location            = azurerm_resource_group.aiof_rg.location
+  resource_group_name = azurerm_resource_group.aiof_rg.name
+
+  tags = {
+    env = var.env
+  }
+}
+
+resource "azurerm_network_security_rule" "aiof_vnet_nsg_rule" {
+  name                        = "DatabaseAdminInbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "80,443"
+  source_address_prefix       = var.db_admin_start_ip
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.aiof_rg.name
+  network_security_group_name = azurerm_network_security_group.aiof_vnet_nsg.name
+}
+
+resource "azurerm_network_ddos_protection_plan" "aiof_ddos_pp" {
+  name                = "vnet-ddos-pp-${var.env}-${var.location}"
+  location            = azurerm_resource_group.aiof_rg.location
+  resource_group_name = azurerm_resource_group.aiof_rg.name
+
+  tags = {
+    env = var.env
+  }
+}
+
+resource "azurerm_virtual_network" "aiof_vnet" {
+  name                = "vnet-${var.env}-${var.location}"
+  location            = azurerm_resource_group.aiof_rg.location
+  resource_group_name = azurerm_resource_group.aiof_rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  ddos_protection_plan {
+    id     = azurerm_network_ddos_protection_plan.aiof_ddos_pp.id
+    enable = true
+  }
+
+  subnet {
+    name           = "backends"
+    address_prefix = "10.2.3.0/24"
+    security_group = azurerm_network_security_group.aiof_vnet_nsg.id
+  }
+
+  tags = {
+    env = var.env
+  }
+}
+
+
+
 resource "azurerm_postgresql_server" "aiof_postgres_server" {
   name                = "aiof-${var.env}"
   location            = azurerm_resource_group.aiof_rg.location
@@ -48,12 +107,4 @@ resource "azurerm_postgresql_database" "aiof_postgres_db" {
   server_name         = azurerm_postgresql_server.aiof_postgres_server.name
   charset             = "UTF8"
   collation           = "English_United States.1252"
-}
-
-resource "azurerm_postgresql_firewall_rule" "example" {
-  name                = "ClientIPAddressAdmin"
-  resource_group_name = azurerm_resource_group.aiof_rg.name
-  server_name         = azurerm_postgresql_server.aiof_postgres_server.name
-  start_ip_address    = var.db_admin_start_ip
-  end_ip_address      = var.db_admin_start_ip
 }
