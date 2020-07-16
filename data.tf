21337e6dea7b2ba3b8c1aff2812ae79be085164d
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-16 15:15:13 -0400

    update postgres db

--- data.tf
+++ data.tf
@@ -35,18 +35,21 @@ resource "azurerm_postgresql_server" "aiof_postgres_server" {
   name                = "aiof-${var.env}"
   location            = azurerm_resource_group.aiof_rg.location
   resource_group_name = azurerm_resource_group.aiof_rg.name
+ 
+  administrator_login          = var.db_admin_username
+  administrator_login_password = var.db_admin_password
 
-  sku_name = "B_Gen5_2"
+  sku_name   = "B_Gen5_2"
+  version    = "9.5"
+  storage_mb = 5120
 
-  storage_mb                   = 5120
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
   auto_grow_enabled            = false
 
-  administrator_login          = var.db_admin_username
-  administrator_login_password = var.db_admin_password
-  version                      = "9.5"
-  ssl_enforcement_enabled      = true
+  public_network_access_enabled    = true
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
 
   tags = {
     env = var.env
