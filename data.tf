137bc529674b1890f3d78c3fe20b135e6254e409
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-16 15:00:55 -0400

    add postgres database

---
+++ data.tf
@@ -0,0 +1,62 @@
+variable subscription_id {}
+variable tenant_id {}
+variable client_id {}
+variable client_secret {}
+variable location {}
+variable env {}
+
+variable db_admin_username {}
+variable db_admin_password {}
+
+
+provider "azurerm" {
+ version = "~> 2.19.0"
+
+ subscription_id = var.subscription_id
+ tenant_id = var.tenant_id
+ client_id = var.client_id
+ client_secret = var.client_secret
+
+ features {}
+}
+
+
+resource "azurerm_resource_group" "aiof_rg" {
+  name     = "aiof-${var.env}"
+  location = var.location
+  
+  tags = {
+    env = var.env
+  }
+}
+
+
+resource "azurerm_postgresql_server" "aiof_postgres_server" {
+  name                = "aiof-${var.env}"
+  location            = azurerm_resource_group.aiof_rg.location
+  resource_group_name = azurerm_resource_group.aiof_rg.name
+
+  sku_name = "B_Gen5_2"
+
+  storage_mb                   = 5120
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  auto_grow_enabled            = false
+
+  administrator_login          = var.db_admin_username
+  administrator_login_password = var.db_admin_password
+  version                      = "9.5"
+  ssl_enforcement_enabled      = true
+
+  tags = {
+    env = var.env
+  }
+}
+
+resource "azurerm_postgresql_database" "example" {
+  name                = "AIOF"
+  resource_group_name = azurerm_resource_group.aiof_rg.name
+  server_name         = azurerm_postgresql_server.aiof_postgres_server.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+}
