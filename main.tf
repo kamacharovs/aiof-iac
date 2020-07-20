6c210685dc956334145dd9ecdb067339bd8caaf5
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-20 13:17:45 -0400

    update to add initial kubernetes cluster

--- main.tf
+++ main.tf
@@ -84,8 +84,8 @@ resource "azurerm_subnet" "aiof_backends" {
   service_endpoints    = ["Microsoft.Sql"]
 }
 
-resource "azurerm_subnet" "aiof_kube" {
-  name                 = "kube"
+resource "azurerm_subnet" "aiof_aksnodes" {
+  name                 = "aksnodes"
   resource_group_name  = azurerm_resource_group.aiof_rg.name
   virtual_network_name = azurerm_virtual_network.aiof_vnet.name
   address_prefixes     = ["10.240.0.0/16"]
@@ -154,4 +154,40 @@ resource "azurerm_postgresql_virtual_network_rule" "example" {
   server_name                          = azurerm_postgresql_server.aiof_postgres_server.name
   subnet_id                            = azurerm_subnet.aiof_backends.id
   ignore_missing_vnet_service_endpoint = true
+}
+
+
+
+/*
+ * Kubernetes
+ */
+resource "azurerm_kubernetes_cluster" "aiof_aks" {
+  name                = "aiof-${var.env}"
+  location            = azurerm_resource_group.aiof_rg.location
+  resource_group_name = azurerm_resource_group.aiof_rg.name
+  dns_prefix          = "aiof-${var.env}"
+  kubernetes_version  = 1.17
+  sku_tier            = "Free"
+
+  default_node_pool {
+    name                  = "default"
+    node_count            = 1
+    vm_size               = "Standard_B2s"
+    os_disk_size_gb       = 8
+    enable_node_public_ip = true
+
+    vnet_subnet_id  = azurerm_subnet.aiof_aksnodes.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    env = var.env
+  }
 }
