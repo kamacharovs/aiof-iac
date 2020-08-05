47c34a0ea2b56cf4f5dba0fdad10591b708749b6
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-05 09:43:45 -0400

    update to move kube out of main

--- main.tf
+++ main.tf
@@ -206,39 +206,3 @@ resource "azurerm_app_service" "aiof_auth" {
     value = var.appsettings_connection_string_database_value
   }
 }
-
-
-
-/*
- * Kubernetes
- */
-resource "azurerm_kubernetes_cluster" "aiof_aks" {
-  name                = "aiof-${var.env}"
-  location            = azurerm_resource_group.aiof_rg.location
-  resource_group_name = azurerm_resource_group.aiof_rg.name
-  dns_prefix          = "aiof-${var.env}"
-  kubernetes_version  = var.kubernetes_version
-  sku_tier            = "Free"
-
-  default_node_pool {
-    name                  = "default"
-    node_count            = 1
-    vm_size               = "Standard_B2s"
-    os_disk_size_gb       = 8
-    enable_node_public_ip = true
-
-    vnet_subnet_id  = azurerm_subnet.aiof_aksnodes.id
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  role_based_access_control {
-    enabled = true
-  }
-
-  tags = {
-    env = var.env
-  }
-}
