98e652241d377e1fb7f82a551378d8161f81bb50
Georgi Kamacharov <gkamacharov@gmail.com>
2021-01-11 12:25:54 -0500

    update network with vn and subnets

--- network.tf
+++ network.tf
@@ -1,9 +1,47 @@
 /*
  * Networking
- * - Network security group with rule for IP of DB Admin
  * - Virtual network
  * - Subnet
+ * - Network security group
  */
+resource "azurerm_virtual_network" "aiof_vnet" {
+  name                = "vnet-${var.location}-${var.env}"
+  location            = azurerm_resource_group.aiof_rg.location
+  resource_group_name = azurerm_resource_group.aiof_rg.name
+  address_space       = ["10.2.0.0/16"]
+
+  tags = {
+    env = var.env
+  }
+}
+
+resource "azurerm_subnet" "backends" {
+  name                 = "backends"
+  resource_group_name  = azurerm_resource_group.aiof_rg.name
+  virtual_network_name = azurerm_virtual_network.aiof_vnet.name
+  address_prefixes     = ["10.2.3.0/24"]
+
+  service_endpoints = [
+    "Microsoft.Sql",
+    "Microsoft.Storage",
+    "Microsoft.KeyVault",
+    "Microsoft.ServiceBus"
+  ]
+}
+
+resource "azurerm_subnet" "databases" {
+  name                 = "databases"
+  resource_group_name  = azurerm_resource_group.aiof_rg.name
+  virtual_network_name = azurerm_virtual_network.aiof_vnet.name
+  address_prefixes     = ["10.2.4.0/24"]
+}
+
+resource "azurerm_subnet" "aksnodes" {
+  name                 = "aksnodes"
+  resource_group_name  = azurerm_resource_group.aiof_rg.name
+  virtual_network_name = azurerm_virtual_network.aiof_vnet.name
+  address_prefixes     = ["10.240.0.0/16"]
+}
 /*
 resource "azurerm_network_security_group" "aiof_vnet_nsg" {
   name                = "vnet-${var.env}-${var.location}-nsg"
@@ -27,31 +65,5 @@ resource "azurerm_network_security_rule" "aiof_vnet_nsg_rule" {
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.aiof_rg.name
   network_security_group_name = azurerm_network_security_group.aiof_vnet_nsg.name
-}
-
-resource "azurerm_virtual_network" "aiof_vnet" {
-  name                = "vnet-${var.location}-${var.env}"
-  location            = azurerm_resource_group.aiof_rg.location
-  resource_group_name = azurerm_resource_group.aiof_rg.name
-  address_space       = ["10.0.0.0/8"]
-
-  tags = {
-    env = var.env
-  }
-}
-
-resource "azurerm_subnet" "aiof_backends" {
-  name                 = "backends"
-  resource_group_name  = azurerm_resource_group.aiof_rg.name
-  virtual_network_name = azurerm_virtual_network.aiof_vnet.name
-  address_prefixes     = ["10.2.3.0/24"]
-  service_endpoints    = ["Microsoft.Sql"]
-}
-
-resource "azurerm_subnet" "aiof_aksnodes" {
-  name                 = "aksnodes"
-  resource_group_name  = azurerm_resource_group.aiof_rg.name
-  virtual_network_name = azurerm_virtual_network.aiof_vnet.name
-  address_prefixes     = ["10.240.0.0/16"]
 }
 */
