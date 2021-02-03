735a59ef15342cfed13c2ea47066896b7a818d19
Georgi Kamacharov <gkamacharov@gmail.com>
2021-02-03 11:47:48 -0500

    update to add key (state name)

--- backends.tf
+++ backends.tf
@@ -3,5 +3,8 @@ terraform {
     resource_group_name  = "aiof-iac"
     storage_account_name = "aiofiacdev"
     container_name       = "aiofiaccd"
+    key                  = "tf/terraform.tfstate"
+
+    # Do not set access_key here. Please set a command line variable TF_VAR_access_key
   }
 }
