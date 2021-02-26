11cbf00cb2daa6e95e5f9147446bc5b3f87d5aa5
Georgi Kamacharov <gkamacharov@gmail.com>
2021-02-26 10:09:52 -0500

    add project

--- github/main.tf
+++ github/main.tf
@@ -12,6 +12,12 @@ provider "github" {
   organization  = var.organization
 }
 
+
+resource "github_organization_project" "aiof" {
+  name = "aiof"
+  body = "All in one finance project"
+}
+
 resource "github_team" "devs" {
   name        = "devs"
   description = "Developers"
