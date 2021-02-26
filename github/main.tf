f49cbee06ec2067ae2ce84d1491c6630ce4bcf15
Georgi Kamacharov <gkamacharov@gmail.com>
2021-02-26 13:50:32 -0500

    add metadata and separate repos

--- github/main.tf
+++ github/main.tf
@@ -13,11 +13,6 @@ provider "github" {
 }
 
 
-locals {
-  master_branch = "master"
-  main_branch   = "main"
-}
-
 resource "github_organization_project" "aiof" {
   name = "aiof"
   body = "All in one finance project"
@@ -27,26 +22,4 @@ resource "github_team" "devs" {
   name        = "devs"
   description = "Developers"
   privacy     = "closed"
-}
-
-resource "github_repository" "aiof_messaging" {
-  name        = "aiof-messaging"
-  description = "All in one finance messaging microservice"
-
-  visibility    = "public"
-  has_issues    = true
-  has_projects  = true
-  has_wiki      = true
-  has_downloads = true
-
-  allow_merge_commit  = false
-  allow_rebase_merge  = false
-  allow_squash_merge  = false
-
-  topics = [
-    "azure-functions-v3",
-    "azure-service-bus",
-    "csharp",
-    "netcore31"
-  ]
 }
---
+++ github/repos.tf
@@ -0,0 +1,47 @@
+resource "github_repository" "aiof_metadata" {
+  name        = "aiof-metadata"
+  description = "All in one finance data crunching backend API"
+
+  visibility          = "public"
+  has_issues          = true
+  has_projects        = true
+  has_wiki            = true
+  has_downloads       = true
+  archive_on_destroy  = true
+
+  allow_merge_commit  = false
+  allow_rebase_merge  = false
+  allow_squash_merge  = false
+
+  topics = [
+    "docker",
+    "finance",
+    "python",
+    "uvicorn",
+    "azure-pipelines",
+    "fastapi"
+  ]
+}
+
+resource "github_repository" "aiof_messaging" {
+  name        = "aiof-messaging"
+  description = "All in one finance messaging microservice"
+
+  visibility          = "public"
+  has_issues          = true
+  has_projects        = true
+  has_wiki            = true
+  has_downloads       = true
+  archive_on_destroy  = true
+
+  allow_merge_commit  = false
+  allow_rebase_merge  = false
+  allow_squash_merge  = false
+
+  topics = [
+    "azure-functions-v3",
+    "azure-service-bus",
+    "csharp",
+    "netcore31"
+  ]
+}
