7e71f0100ff29e0b34efa23e320ec6c04816da14
Georgi Kamacharov <gkamacharov@gmail.com>
2021-03-10 09:25:40 -0500

    add users

---
+++ github/users.tf
@@ -0,0 +1,9 @@
+data "github_user" "dkamacharov19" {
+  username = "dkamacharov19"
+}
+
+resource "github_team_membership" "dkamacharov19_devs_membership" {
+  team_id  = github_team.devs.id
+  username = data.github_user.dkamacharov19.username
+  role     = "member"
+}
