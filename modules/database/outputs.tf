5a08e2eee4b0b5b961ffaff3ee1fe84099dc5300
Georgi Kamacharov <gkamacharov@gmail.com>
2021-04-22 13:15:13 -0400

    update output variables

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -1,4 +1,14 @@
+output "server_name" {
+    value       = azurerm_postgresql_server.aiof_postgres_server.fqdn
+    description = "Server name"
+}
+
+output "database_name" {
+    value       = azurerm_postgresql_database.aiof_postgres_db.name
+    description = "Database name"
+}
+
 output "database_connection_string" {
-  value       = "Server=${azurerm_postgresql_server.aiof_postgres_server.fqdn};Database=${azurerm_postgresql_database.aiof_postgres_db.name};Port=5432;User Id=${var.db_admin_username};Password=${var.db_admin_password};Ssl Mode=Require;"
+  value       = "Server=${var.server_name};Database=${azurerm_postgresql_database.aiof_postgres_db.name};Port=5432;User Id=${var.db_admin_username}@${azurerm_postgresql_database.aiof_postgres_db.name};Password=${var.db_admin_password};Ssl Mode=Require;"
   description = "Database connection string"
 }
