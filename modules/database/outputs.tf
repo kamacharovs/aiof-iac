7ee92134cff5af41a4cd55c0fa3b0938836bfa27
Georgi Kamacharov <gkamacharov@gmail.com>
2021-04-22 15:43:28 -0400

    update to server name

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -1,5 +1,5 @@
 output "database_connection_string" {
-  value       = "Server=${azurerm_postgresql_server.aiof_postgres_server.fqdn};Database=${azurerm_postgresql_database.aiof_postgres_db.name};Port=5432;User Id=${var.db_admin_username}@${azurerm_postgresql_server.aiof_postgres_server.fqdn};Password=${var.db_admin_password};Ssl Mode=Require;"
+  value       = "Server=${azurerm_postgresql_server.aiof_postgres_server.fqdn};Database=${azurerm_postgresql_database.aiof_postgres_db.name};Port=5432;User Id=${var.db_admin_username}@${azurerm_postgresql_server.aiof_postgres_server.name};Password=${var.db_admin_password};Ssl Mode=Require;"
   description = "Database connection string"
   sensitive   = true
 }
